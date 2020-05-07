93713b37e72f4c653331648fedd3b9c449f3c7df
Kurtil <gaetan.lagier@hotmail.fr>
2020-05-07 15:44:47 +0200

    working test environment

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  moduleFileExtensions: [
+    "js",
+    "vue"
+  ],
+  transform: {
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+    ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest"
+  }
+}
