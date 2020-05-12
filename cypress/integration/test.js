d9d3d9e9ef1af13b638a0396c7a0b2415a7fb8d2
Kurtil <gaetan.lagier@hotmail.fr>
2020-05-12 15:34:46 +0200

    cypress test test.html in cypress/pages

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,5 +1,21 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+describe('Starting with simple area', () => {
+  beforeEach(() => {
+    cy.visit('test');
+    const cfg = {
+      components: [{ render(h) { return h("div", "Hey !") } }],
+      layout: {
+        componentIndex: 0
+      }
+    };
+    cy.window().then(win => {
+      win.areas = win.mountApp(cfg);
+    });
+  });
+
+  it('Should render the simple area', () => {
+    cy.get("#window-1").contains("Hey !");
+    cy.window().then(win => {
+      cy.get("#app").then(app => expect(win.areas.$el).to.equal(app))
+    })
   });
 });
