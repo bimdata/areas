04be09b35fcce6689700d661e375444012baa16b
Kurtil <gaetan.lagier@hotmail.fr>
2020-05-13 15:11:15 +0200

    update test unit jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+  rootDir: "unitTest",
   moduleFileExtensions: [
     "js",
     "vue"
   ],
   transform: {
-    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
-    ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest"
+    "^.+\\.js$": "<rootDir>/../node_modules/babel-jest",
+    ".*\\.(vue)$": "<rootDir>/../node_modules/vue-jest"
   }
 }
